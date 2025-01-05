1053650e0cccb9d6c3340a38d19fe54915448d00
Cida Rosa <lucita.rosa@gmail.com>
2025-01-05 15:41:10 -0300

    implementado funções-não altera o visual do componente

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,29 @@
+import { findProduct } from "./data";
+
 function App() {
-  
+
+  console.log("Passou no App");
+  // FORMA ERRADA
+  // eslint-disable-next-line prefer-const
+  let productId = 1;
+  // eslint-disable-next-line prefer-const
+  let prod = findProduct(productId);
+
+  function handleClick() {
+    console.log("PASSOU NO handleClick");
+    productId = 2;
+    prod = findProduct(productId);
+  }
 
   return (
-    <h1>Olá</h1>
+    <>
+      <h2>{prod?.name}</h2>
+      <p>{prod?.price}</p>
+
+      <button onClick={handleClick}> 
+        Mudar para produto 2
+      </button>
+    </>
   )
 }
 
